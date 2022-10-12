36ec87c83d1f50ab0b0f4a471e9d3b8fd655ab90
surfaceflinger <nat@nekopon.pl>
2022-10-12 15:54:11 +0200

    lol Xd

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import requests
+from telegram import Bot, Chat, ParseMode
+
+with open('/var/xkom/settings.txt', 'r') as file:
+    file = file.readlines()
+    token = file[0].rstrip()
+    channel = file[1].rstrip()
+
+apiheaders = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
+    "X-API-Key": "sJSgnQXySmp6pqNV",
+}
+
+apiurl = "https://mobileapi.x-kom.pl/api/v1/xkom/hotShots/current?onlyHeader=false"
+
+def getHotShot():
+    r = requests.get(url=apiurl, headers=apiheaders).json()
+    return([r['PromotionName'], r['Price'], r['OldPrice'], "https://www.x-kom.pl/goracy_strzal/"+r['Id'], r['Product']['MainPhoto']['Url']])
+
+def main():
+    bot = Bot(token)
+    epic = getHotShot()
+    description = "Item: {}\nCena: {}PLN\nStara cena: {}PLN\nLink: {}".format(epic[0], epic[1], epic[2], epic[3])
+    try:
+        bot.send_photo(chat_id=channel, photo=epic[4], caption=description)
+    except:
+        bot.send_message(chat_id=channel, text=description)
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,8 @@
+# setup.py
+from distutils.core import setup
+
+setup(
+    name='xkomhotshot',
+    version='0.0.1',
+    scripts=['main.py',],
+)
