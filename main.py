81cd63479ed0e8f3c3203985d58c73b8e2f2357a
surfaceflinger <nat@nekopon.pl>
2023-08-15 14:05:21 +0200

    cooler code

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python3
 import asyncio
 import requests
-from telegram import Bot, Chat
+from telegram import Bot
 from telegram.constants import ParseMode
 
+# Load API credentials from the settings file
 with open('/var/lib/xkomhotshot/settings.txt', 'r') as file:
-    file = file.readlines()
-    token = file[0].rstrip()
-    channel = file[1].rstrip()
+    lines = file.readlines()
+    token = lines[0].strip()
+    channel = lines[1].strip()
 
-apiheaders = {
+API_HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
     "X-API-Key": "sJSgnQXySmp6pqNV",
 }
 
-apiurl = "https://mobileapi.x-kom.pl/api/v1/xkom/hotShots/current?onlyHeader=false"
+API_URL = "https://mobileapi.x-kom.pl/api/v1/xkom/hotShots/current?onlyHeader=false"
 
-def getHotShot():
-    r = requests.get(url=apiurl, headers=apiheaders).json()
-    return([r['PromotionName'], r['Price'], r['OldPrice'], "https://www.x-kom.pl/goracy_strzal/"+r['Id'], r['Product']['MainPhoto']['Url']])
+def get_hot_shot():
+    response = requests.get(url=API_URL, headers=API_HEADERS).json()
+    hot_shot_info = {
+        "name": response['PromotionName'],
+        "price": response['Price'],
+        "old_price": response['OldPrice'],
+        "link": f"https://www.x-kom.pl/goracy_strzal/{response['Id']}",
+        "photo_url": response['Product']['MainPhoto']['Url'],
+    }
+    return hot_shot_info
 
 async def main():
     bot = Bot(token)
-    epic = getHotShot()
-    description = "Item: {}\nCena: {}PLN\nStara cena: {}PLN\nLink: {}".format(epic[0], epic[1], epic[2], epic[3])
+    hot_shot_data = get_hot_shot()
+    description = (
+        f"Item: {hot_shot_data['name']}\n"
+        f"Cena: {hot_shot_data['price']} PLN\n"
+        f"Stara cena: {hot_shot_data['old_price']} PLN\n"
+        f"Link: {hot_shot_data['link']}"
+    )
     try:
-       await bot.send_photo(chat_id=channel, photo=epic[4], caption=description)
-    except:
-       await bot.send_message(chat_id=channel, text=description)
+        await bot.send_photo(chat_id=channel, photo=hot_shot_data['photo_url'], caption=description, parse_mode=ParseMode.MARKDOWN)
+    except Exception as e:
+        await bot.send_message(chat_id=channel, text=description)
 
 if __name__ == '__main__':
-   asyncio.run(main())
+    asyncio.run(main())
